5ae5a49eaa3fc618ea16e36041953d5bec7904aa
Sankar491 <sivasankarrcks@gmail.com>
2024-12-18 22:36:01 +0530

    Terraform

---
+++ Ec2/main.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "test" {
+  ami           = "ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+
+  tags = {
+    Name = "Test"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
