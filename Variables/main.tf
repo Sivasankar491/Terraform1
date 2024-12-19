5a13742f96b0064fc1a20ab852793bae45010a27
Sankar491 <sivasankarrcks@gmail.com>
2024-12-19 10:50:06 +0530

    Terraform

---
+++ Variables/main.tf
@@ -0,0 +1,39 @@
+resource "aws_instance" "test" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id ]
+
+  tags = merge(
+    var.common_tags,
+        {
+            Name = "Test"
+        }
+  ` ) 
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+    tags = merge(
+        var.common_tags,
+            {
+                Name = "Test"
+            }
+  `      )
+}
---
+++ Variables/variables.tf
@@ -0,0 +1,19 @@
+variable "ami" {
+    type = string
+    default = "ami-09c813fb71547fc4f" 
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+}
+
+
+variable "common_tags" {
+    type = map
+    default = {
+        Environment = "UAT"
+        component = "Backend"
+    }
+  
+}
