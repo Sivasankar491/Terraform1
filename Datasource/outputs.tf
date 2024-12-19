0768abd7fd65c0a90bd8a3b373379a91568ddfb9
Sankar491 <sivasankarrcks@gmail.com>
2024-12-19 21:52:47 +0530

    Terraform

--- Datasource/outputs.tf
+++ Datasource/outputs.tf
@@ -1,3 +1,7 @@
-output "instance_info" {
-    value = aws_instance.expense
+output "instance_private_ip" {
+    value = aws_instance.expense.private_ip
+}
+
+output "instance_public_ip" {
+    value = aws_instance.expense.public_ip
 }
