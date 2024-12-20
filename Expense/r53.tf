103c26d5cdb8304f83d41c76e465e8bfdd27db7c
Sankar491 <sivasankarrcks@gmail.com>
2024-12-20 22:30:34 +0530

    terraform

--- Expense/r53.tf
+++ Expense/r53.tf
@@ -1,5 +1,6 @@
 resource "aws_route53_record" "expense" {
-  count = length(var.instance_names)  
+  # count = length(var.instance_names) 
+  count = length(aws_instance.expense) 
   zone_id = data.aws_route53_zone.expense.zone_id
   name    = var.instance_names[count.index] == "frontend" ? var.zone_name : "${var.instance_names[count.index]}.${var.zone_name}"
   type    = "A"
