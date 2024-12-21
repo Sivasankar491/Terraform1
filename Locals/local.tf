197a75d7a6b123a24147f001c1cbc6749edc30d3
Sankar491 <sivasankarrcks@gmail.com>
2024-12-21 12:26:45 +0530

    terraform

--- Locals/local.tf
+++ Locals/local.tf
@@ -1,3 +1,6 @@
 locals {
   instance_type = var.environment == "prod" ? "t2.small" : "t3.micro"
 }
+
+#The "count" object can only be used in "module", "resource", and "data" blocks, and only when the "count" argument is set.
+ 
